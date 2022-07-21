d570904082133edf3d44d90a7d10c93a02b04f6e
Keven C <keven_jkc@hotmail.com>
2022-07-21 11:28:12 -0300

    uploading

---
+++ basic.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+ 
+/*
+Calculadora IMC
+int main(){
+    float imc,peso,altura;
+
+    printf("Digite a altura:");
+    scanf(" %f", &altura);
+
+    printf("Digite o peso:");
+    scanf(" %f", &peso);
+
+    imc = peso/(altura *altura);
+    printf("IMC = %.2f", imc);
+    return 0;
+}
+*/
